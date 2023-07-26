b8c2694276e12678de5fb96c88296c2d7d04aebe
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-07-26 22:33:25 +0200

    typo + cdn tailwind

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+<template>
+
+    <div>
+        <img alt="Nbys logo" class="h-32 w-32" src="./../assets/img/logo.svg" />
+    
+        <div class="wrapper">
+        </div>
+    </div>
+</template>
