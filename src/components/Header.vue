fc7c00fdc1a7f0cd42977ed84d3ac7f8f92543b8
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-08-03 16:28:32 +0200

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,37 @@
 <script setup>
 </script>
 <template>
-
-    <div>
-        <img alt="Nbys logo" class="h-32 w-32" src="./../assets/img/logo.svg" />
-    
-        <div class="wrapper">
+    <div class="mx-auto">
+      <div class="flex justify-between items-center">
+        <img alt="Nbys logo" class="z-20 w-28 h-auto" src="./../assets/img/logo.svg" />
+        <div class="lg:hidden">
+            <div class="">
+                <img  v-if="!menuOpen" @click="toggleMenu" src="./../assets/img/burger.svg" alt="NbyS - icon Burger">
+                <img  v-if="menuOpen" @click="toggleMenu" src="./../assets/img/close.svg" alt="NbyS - icon Close">
+            </div>
         </div>
+      </div>
+      <div v-if="menuOpen" class="lg:hidden z-10 h-screen flex items-center justify-center">
+          <ul class="flex flex-col gap-10">
+            <li v-for="(menuItem, index) in menuItems" :key="index" class="text-2xl hover:text-white">
+              {{ menuItem }}
+            </li>
+          </ul>
+      </div>   
     </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      menuOpen: false,
+      menuItems: ["Home", "About", "Contact"],
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen;
+    },
+  },
+};
+</script>
